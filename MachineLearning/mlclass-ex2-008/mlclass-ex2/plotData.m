ef4177e00c27ed8a36517971e45c2ca124d09734
Zachary Radtka <zachary.radtka@gmail.com>
2015-02-16 12:14:25 +0100

    ZER - Updated plotData.m to plot datapoints

--- MachineLearning/mlclass-ex2-008/mlclass-ex2/plotData.m
+++ MachineLearning/mlclass-ex2-008/mlclass-ex2/plotData.m
@@ -12,11 +12,13 @@ figure; hold on;
 %               examples and 'ko' for the negative examples.
 %
 
+% Find the indecies of the Positive and Negative examples
+pos = find(y == 1);
+neg = find(y == 0);
 
-
-
-
-
+% Plot the examples
+plot(X(pos, 1), X(pos, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
 
 
