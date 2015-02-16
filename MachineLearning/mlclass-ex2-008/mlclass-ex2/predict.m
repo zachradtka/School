882f8857fc799d734d93ff24bc2edf52c1f1aaf6
Zachary Radtka <zachary.radtka@gmail.com>
2015-02-16 15:48:46 +0100

    ZER - completed predictions

--- MachineLearning/mlclass-ex2-008/mlclass-ex2/predict.m
+++ MachineLearning/mlclass-ex2-008/mlclass-ex2/predict.m
@@ -15,9 +15,11 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+% Compute the predictions
+predictions = sigmoid(X * theta);
 
-
-
+% Determine the predicted outcome
+p = round(predictions);
 
 
 
