56c6b12be4c4188724538e10b27a785534ada950
Zachary Radtka <zachary.radtka@gmail.com>
2015-02-17 10:31:42 +0100

    ZER - Completed the exercises

--- MachineLearning/mlclass-ex2-008/mlclass-ex2/costFunctionReg.m
+++ MachineLearning/mlclass-ex2-008/mlclass-ex2/costFunctionReg.m
@@ -17,10 +17,21 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% Compute the cost and gradient 
+[cost_non_reg, grad_non_reg] = costFunction(theta, X, y);
 
+% Don't penalize theta0, in octave theta(1) is theta0
+theta_reg = [0; theta([2:length(theta)],:)];
 
+% Compute the regularization
+reg = lambda / (2 * m) * (theta_reg' * theta_reg);
 
+% Add the regularization terms to the cost
+J = cost_non_reg + reg;
 
+grad_reg = (lambda / m) * (theta_reg);
+
+grad = grad_non_reg + grad_reg;
 
 % =============================================================
 
