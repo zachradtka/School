ef522344b47ef7da2dbe405c924706a3e4117831
Zachary Radtka <zachary.radtka@gmail.com>
2015-03-03 15:35:45 +0100

    ZER - Completed assignment

--- MachineLearning/mlclass-ex4-008/mlclass-ex4/nnCostFunction.m
+++ MachineLearning/mlclass-ex4-008/mlclass-ex4/nnCostFunction.m
@@ -115,9 +115,9 @@ delta_2 = d3' * a2;
 % Compute the gradient for d3
 delta_1 = d2' * a1;
 
-Theta1_grad = (1/m) * delta_1;
-Theta2_grad = (1/m) * delta_2;
-
+% Compute the gradients for theta
+Theta1_grad = (1/m) * delta_1 + (lambda / m) * [zeros(size(Theta1, 1), 1) Theta1(:, (2:end))];
+Theta2_grad = (1/m) * delta_2 + (lambda / m) * [zeros(size(Theta2, 1), 1) Theta2(:, (2:end))];
 
 
 % -------------------------------------------------------------
