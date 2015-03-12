fb2fe4713ec6c403c3176ed7aad531c9d5277122
Zachary Radtka <zachary.radtka@gmail.com>
2015-03-12 11:48:41 +0100

    ZER - Completed the validation curve

--- MachineLearning/mlclass-ex5-008/mlclass-ex5/validationCurve.m
+++ MachineLearning/mlclass-ex5-008/mlclass-ex5/validationCurve.m
@@ -39,14 +39,18 @@ error_val = zeros(length(lambda_vec), 1);
 %
 %
 
+for i = 1:length(lambda_vec)
 
+    lambda = lambda_vec(i);
 
+    [theta] = trainLinearReg(X, y, lambda);
 
+    % Compute J Train for i examples
+    error_train(i) = linearRegCostFunction(X, y, theta, 0);
 
-
-
-
-
+    % Compute the CV error with theat for i examples
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 % =========================================================================
 
