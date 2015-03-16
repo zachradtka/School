593a5a9baa924f9facecdf505d328947a5bdb81c
Zachary Radtka <zachary.radtka@gmail.com>
2015-03-16 11:25:09 +0100

    ZER - Completed the second part of the assignment

--- MachineLearning/mlclass-ex6-008/mlclass-ex6/dataset3Params.m
+++ MachineLearning/mlclass-ex6-008/mlclass-ex6/dataset3Params.m
@@ -9,7 +9,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -24,8 +24,38 @@ sigma = 0.3;
 %
 
 
+C_vec     = [0.01 0.03 0.1 0.3 1 3 10]';
+sigma_vec = [0.01 0.03 0.1 0.3 1 3 10]';
 
 
+min_error = 1;
+best_params = [1 1];
+
+x1 = [1 2 1]; x2 = [0 4 -1];
+
+
+for i = 1:length(C_vec)
+    for j = 1:length(sigma_vec)
+        model= svmTrain(X, y, C_vec(i), @(x1, x2) gaussianKernel(x1, x2, sigma_vec(j)));
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
+
+        fprintf('C: %f\tsigma: %f\terror: %f\n', C_vec(i), sigma_vec(j), error);
+
+        if error < min_error
+            min_error = error;
+            best_params = [i j];
+        end
+    end
+end
+fprintf('Values being used are\n');
+fprintf('C is: %f\n', C_vec(best_params(1)));
+fprintf('sigma is: %f\n', sigma_vec(best_params(2)));
+fprintf('Error is: %f\n', min_error);
+
+
+C = C_vec(best_params(1));
+sigma = sigma_vec(best_params(2));
 
 
 
---
+++ MachineLearning/mlclass-ex6-008/mlclass-ex6/ex6_temp.m
@@ -0,0 +1,28 @@
+
+%% Initialization
+clear ; close all; clc
+
+x1 = [1 2 1]; x2 = [0 4 -1];
+
+%% ========== Part 7: Training SVM with RBF Kernel (Dataset 3) ==========
+
+%  This is a different dataset that you can use to experiment with. Try
+%  different values of C and sigma here.
+% 
+
+% Load from ex6data3: 
+% You will have X, y in your environment
+load('ex6data3.mat');
+
+% Try different SVM Parameters here
+[C, sigma] = dataset3Params(X, y, Xval, yval);
+fprintf('Values being used are\n');
+fprintf('C is: %f\n', C);
+fprintf('sigma is: %f\n', sigma);
+
+% Train the SVM
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+visualizeBoundary(X, y, model);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
